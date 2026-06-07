0779b4798ec42ae208c898ee53e06f5e36d5474b
pinktok008-hue <pinktok008@gmail.com>
2026-06-07 18:31:24 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+import os
 
 app = FastAPI()
 
+UPLOAD_FOLDER = "uploads"
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
 @app.get("/")
 def home():
+    return {"message": "AI Video Dubbing API Running"}
+
+@app.post("/upload")
+async def upload_video(video: UploadFile = File(...)):
+    file_path = os.path.join(UPLOAD_FOLDER, video.filename)
+
+    with open(file_path, "wb") as f:
+        f.write(await video.read())
+
     return {
-        "message": "AI Video Dubbing API Running"
+        "status": "success",
+        "filename": video.filename
     }
