a1928f3c3690a762a9f366abde6c6d18faf37194
pinktok008-hue <pinktok008@gmail.com>
2026-06-08 09:08:49 +0530

    Update audio_extractor.py

--- backend/services/audio_extractor.py
+++ backend/services/audio_extractor.py
@@ -1,9 +1,13 @@
 import ffmpeg
 
 def extract_audio(video_path, audio_path):
-    (
-        ffmpeg
-        .input(video_path)
-        .output(audio_path)
-        .run(overwrite_output=True)
-    )
+(
+ffmpeg
+.input(video_path)
+.output(audio_path)
+.run(overwrite_output=True)
+)
+
+```
+return audio_path
+```
