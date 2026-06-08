bc2ca517dd974b841d4a36efe11fa418dd31c1e1
pinktok008-hue <pinktok008@gmail.com>
2026-06-08 09:07:58 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,28 +1,86 @@
 from fastapi import FastAPI, UploadFile, File
 import os
+import subprocess
+from services.audio_extractor import extract_audio
 
 app = FastAPI()
 
 UPLOAD_FOLDER = "uploads"
+AUDIO_FOLDER = "audio"
+
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+os.makedirs(AUDIO_FOLDER, exist_ok=True)
 
 @app.get("/")
 def home():
-    return {
-        "message": "AI Video Dubbing API Running"
-    }
+return {
+"message": "AI Video Dubbing API Running"
+}
 
 @app.post("/upload")
 async def upload_video(video: UploadFile = File(...)):
-    file_path = os.path.join(
-        UPLOAD_FOLDER,
-        video.filename
-    )
+file_path = os.path.join(
+UPLOAD_FOLDER,
+video.filename
+)
 
-    with open(file_path, "wb") as f:
-        f.write(await video.read())
+```
+with open(file_path, "wb") as f:
+    f.write(await video.read())
 
+return {
+    "status": "success",
+    "filename": video.filename
+}
+```
+
+@app.get("/ffmpeg-test")
+def ffmpeg_test():
+try:
+result = subprocess.run(
+["ffmpeg", "-version"],
+capture_output=True,
+text=True
+)
+
+```
     return {
-        "status": "success",
-        "filename": video.filename
+        "installed": True,
+        "output": result.stdout[:300]
     }
+
+except Exception as e:
+    return {
+        "installed": False,
+        "error": str(e)
+    }
+```
+
+@app.post("/extract-audio")
+async def extract_audio_api(video: UploadFile = File(...)):
+video_path = os.path.join(
+UPLOAD_FOLDER,
+video.filename
+)
+
+```
+with open(video_path, "wb") as f:
+    f.write(await video.read())
+
+audio_name = os.path.splitext(video.filename)[0] + ".wav"
+
+audio_path = os.path.join(
+    AUDIO_FOLDER,
+    audio_name
+)
+
+extract_audio(
+    video_path,
+    audio_path
+)
+
+return {
+    "status": "success",
+    "audio_file": audio_name
+}
+```
