698aa4a70e0ffcb7ec1def35ea3cb504b8e26ed5
pinktok008-hue <pinktok008@gmail.com>
2026-06-12 16:59:28 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -13,74 +13,68 @@ os.makedirs(AUDIO_FOLDER, exist_ok=True)
 
 @app.get("/")
 def home():
-return {
-"message": "AI Video Dubbing API Running"
-}
+    return {
+        "message": "AI Video Dubbing API Running"
+    }
 
 @app.post("/upload")
 async def upload_video(video: UploadFile = File(...)):
-file_path = os.path.join(
-UPLOAD_FOLDER,
-video.filename
-)
+    file_path = os.path.join(
+        UPLOAD_FOLDER,
+        video.filename
+    )
 
-```
-with open(file_path, "wb") as f:
-    f.write(await video.read())
+    with open(file_path, "wb") as f:
+        f.write(await video.read())
 
-return {
-    "status": "success",
-    "filename": video.filename
-}
-```
+    return {
+        "status": "success",
+        "filename": video.filename
+    }
 
 @app.get("/ffmpeg-test")
 def ffmpeg_test():
-try:
-result = subprocess.run(
-["ffmpeg", "-version"],
-capture_output=True,
-text=True
-)
+    try:
+        result = subprocess.run(
+            ["ffmpeg", "-version"],
+            capture_output=True,
+            text=True
+        )
 
-```
-    return {
-        "installed": True,
-        "output": result.stdout[:300]
-    }
+        return {
+            "installed": True,
+            "output": result.stdout[:300]
+        }
 
-except Exception as e:
-    return {
-        "installed": False,
-        "error": str(e)
-    }
-```
+    except Exception as e:
+        return {
+            "installed": False,
+            "error": str(e)
+        }
 
 @app.post("/extract-audio")
 async def extract_audio_api(video: UploadFile = File(...)):
-video_path = os.path.join(
-UPLOAD_FOLDER,
-video.filename
-)
+    video_path = os.path.join(
+        UPLOAD_FOLDER,
+        video.filename
+    )
 
-```
-with open(video_path, "wb") as f:
-    f.write(await video.read())
+    with open(video_path, "wb") as f:
+        f.write(await video.read())
 
-audio_name = os.path.splitext(video.filename)[0] + ".wav"
+    audio_name = os.path.splitext(video.filename)[0] + ".wav"
 
-audio_path = os.path.join(
-    AUDIO_FOLDER,
-    audio_name
-)
+    audio_path = os.path.join(
+        AUDIO_FOLDER,
+        audio_name
+    )
 
-extract_audio(
-    video_path,
-    audio_path
-)
+    extract_audio(
+        video_path,
+        audio_path
+    )
 
-return {
-    "status": "success",
-    "audio_file": audio_name
-}
-```
+    return {
+        "status": "success",
+        "audio_file": audio_name
+    }
