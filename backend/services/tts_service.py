14d537a767dda1ebf7123eb1e689f02e177004f6
pinktok008-hue <pinktok008@gmail.com>
2026-06-29 13:39:34 +0530

    Update tts_service.py

--- backend/services/tts_service.py
+++ backend/services/tts_service.py
@@ -1,39 +1,5 @@
-import os
-import asyncio
 import edge_tts
 
-async def generate_edge_tts(text, language):
-
-    voices = {
-        "Hindi": "hi-IN-SwaraNeural",
-        "English": "en-US-JennyNeural",
-        "German": "de-DE-KatjaNeural",
-        "French": "fr-FR-DeniseNeural",
-        "Spanish": "es-ES-ElviraNeural",
-        "Chinese": "zh-CN-XiaoxiaoNeural",
-        "Japanese": "ja-JP-NanamiNeural",
-        "Korean": "ko-KR-SunHiNeural"
-    }
-
-    voice = voices.get(
-        language,
-        "en-US-JennyNeural"
-    )
-
-    output_file = "audio/generated_voice.mp3"
-
-    communicate = edge_tts.Communicate(
-        text,
-        voice
-    )
-
-    await communicate.save(output_file)
-
-    return output_file
-
-
-import os
-import edge_tts
 
 async def generate_speech(
     text,
