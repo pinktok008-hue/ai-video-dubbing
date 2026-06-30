5a3c6c4f167103381862fbe8e0ec997bd79b2af3
pinktok008-hue <pinktok008@gmail.com>
2026-06-30 14:42:29 +0530

    Update video_merger.py

--- backend/services/video_merger.py
+++ backend/services/video_merger.py
@@ -1,4 +1,5 @@
 import subprocess
+import ffmpeg
 
 
 def merge_video_audio(
@@ -7,22 +8,48 @@ def merge_video_audio(
     output_path
 ):
 
+    # Get durations
+
+    video_info = ffmpeg.probe(video_path)
+    video_duration = float(
+        video_info["format"]["duration"]
+    )
+
+
+    audio_info = ffmpeg.probe(audio_path)
+    audio_duration = float(
+        audio_info["format"]["duration"]
+    )
+
+
+    # Calculate speed
+    speed = audio_duration / video_duration
+
+
     command = [
         "ffmpeg",
         "-i", video_path,
         "-i", audio_path,
 
-        # video length ke hisab se audio cut/adjust
-        "-map", "0:v:0",
-        "-map", "1:a:0",
+        "-filter:a",
+        f"atempo={speed}",
 
-        "-c:v", "copy",
-        "-c:a", "aac",
+        "-map",
+        "0:v:0",
 
-        # original video duration maintain
+        "-map",
+        "1:a:0",
+
+        "-c:v",
+        "copy",
+
+        "-c:a",
+        "aac",
+
         "-shortest",
 
         "-y",
+
         output_path
     ]
 
@@ -35,8 +62,12 @@ def merge_video_audio(
 
 
     if result.returncode != 0:
+
         print(result.stderr.decode())
-        raise Exception("FFmpeg merge failed")
+
+        raise Exception(
+            "Audio speed adjustment failed"
+        )
 
 
     return output_path
